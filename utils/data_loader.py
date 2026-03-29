284ecb74c73578ac6c69668195cb3974a57a1ac2
sabina <sabina.yeasmin@doer.com.bd>
2026-03-29 12:02:18 +0600

    initial commit

--- utils/data_loader.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-
-def load_all_test_data(file_path):
-    data_file = os.path.join(os.path.dirname(__file__),file_path)
-    with open(data_file) as df:
-        return json.load(df)
